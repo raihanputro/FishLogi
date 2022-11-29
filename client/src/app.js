7f67db5f527c27e3bfb98e093cbfb83c7125ad7a
raihanputro <raihanputromaulana477@gmail.com>
2022-11-29 18:20:25 +0700

    Adding index and app .js

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Container } from '@material-ui/core';
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useStyles from './styles';
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+
 
 const App = () => {
     return (
-        <div>
-            Test
-        </div>
-    )
-}
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './app';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+document.getElementById('root').style.fontFamily = "Roboto, sans-serif";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 
