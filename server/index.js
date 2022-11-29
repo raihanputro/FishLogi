86ec87b9f9db5842e57b23073c034f2c1e0d8a0c
raihanputro <raihanputromaulana477@gmail.com>
2022-11-29 18:38:17 +0700

    Adding index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 import fishRoutes from './routes/fishRoutes.js';
+import usersRoutes from './routes/fishRoutes.js'
+
+dotenv.config();
 
 const App = express();
 
@@ -9,5 +14,16 @@ App.use(cors());
 App.use(express.json());
 
 App.use('/fishes', fishRoutes);
+App.use('/users', usersRoutes);
 
-App.listen(5000, () => console.log(`Server berjalan pada http://localhost/5000`));
+App.use(bodyParser.json({ limit: "30mb", extended: true }));
+App.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT;
+
+try {
+    App.listen(PORT, () => console.log(`Server berjalan pada http://localhost/${PORT}`));
+} catch (error) {
+    console.log(`${error}, tidak dapat terhubung!`);
+}
+
