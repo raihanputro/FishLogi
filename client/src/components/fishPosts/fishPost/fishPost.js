256226700d8060f2eca9f19b236d161b38125ff7
raihanputro <raihanputromaulana477@gmail.com>
2022-12-03 20:57:19 +0700

    Adding link from fish post to fish detail post

--- client/src/components/fishPosts/fishPost/fishPost.js
+++ client/src/components/fishPosts/fishPost/fishPost.js
@@ -1,36 +1,43 @@
 import React from "react";
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import moment from 'moment';
+import SettingsIcon from '@material-ui/icons/Settings';
 import { deleteFishPost } from "../../../actions/fishPosts";
 import useStyles from './styles';
 
 const FishPost = ({ fishPost, setCurrentId }) => {
     const dispatch = useDispatch();
     const classes = useStyles();
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const openFishPost = () => {
+        navigate( `/fishes/${fishPost.id}`)
+    }
 
     return (
             <Card className={classes.card}>
-                <CardMedia className={classes.media} image={fishPost.fishPicture || "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"} title={fishPost.name} />
-                <div className={classes.overlay}>
-                    <Typography variant="h6">{fishPost.authorName}</Typography>
-                    <Typography variant="body2">{moment(fishPost.createdAt).fromNow()}</Typography>
-                </div>
-                <div className={classes.overlay2}>
-                    <Button style={{color: 'white'}} size="small" onClick={() => setCurrentId(fishPost.id)}>
-                        <MoreHorizIcon fontSize="medium" />
-                    </Button>
-                </div>
-                <CardContent>
-                    <Typography className={classes.title} variant="h5" gutterBottom>{fishPost.desc}</Typography>
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                    <Button size="small" color="primary">
-                        <DeleteIcon fontSize="small" onClick={() => dispatch(deleteFishPost(fishPost.id))}/>
-                    </Button>
-                </CardActions>
+                <ButtonBase className={classes.cardAction} onClick={openFishPost} component="span">
+                    <CardMedia className={classes.media} image={fishPost.fishPicture || "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"} title={fishPost.name} />  
+                    <Typography style={{ marginTop: '10px'}}className={classes.title} variant="h5">{fishPost.name}</Typography>
+                    <CardContent>
+                        <Typography style={{textAlign: 'justify'}} variant="body2" color="textSecondary" gutterBottom>{fishPost.desc.split(' ').splice(0, 20).join(' ')}...</Typography>
+                    </CardContent>
+                </ButtonBase>
+                {(user?.result?.isAdmin === 'admin') && (
+                    <CardActions className={classes.cardActions}>
+                            <>
+                                <Button size="small" style={{color: 'red'}} onClick={() => dispatch(deleteFishPost(fishPost.id))}>
+                                    <DeleteIcon style={{padding: '0px'}} fontSize="small"/>
+                                </Button>
+                                    <Button color="primary" size="small" onClick={(e) => {e.stopPropagation(); setCurrentId(fishPost.id)}}>
+                                    <SettingsIcon fontSize="medium" />
+                                </Button>
+                            </>
+                    </CardActions>
+                )}
             </Card>
     )
 };
--- client/src/components/fishPosts/fishPost/styles.js
+++ client/src/components/fishPosts/fishPost/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
     media: {
-        height: 0,
+        height: '0',
         paddingTop: '56.25%',
         backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
@@ -48,8 +48,7 @@ export default makeStyles(() => ({
       textAlign: 'initial',
     },
     cardActions: {
-      padding: '0 16px 8px 16px',
-      display: 'flex',
-      justifyContent: 'space-between',
+      padding: '0 16px',
+      margin: 'auto'
     },
 }));
