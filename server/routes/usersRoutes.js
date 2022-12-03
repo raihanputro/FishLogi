d517cc9c5186bba61d4028f410ac76a08f291acf
raihanputro <raihanputromaulana477@gmail.com>
2022-12-03 19:01:11 +0700

    Adding sign in & sign up routes

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import { signIn, signUp } from '../controllers/userController.js';
 
-const routes = express.Router();
+const router = express.Router();
 
+router.post('/signIn', signIn);
+router.post('/signUp', signUp);
+
+export default router;
