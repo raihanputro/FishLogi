de62e67ac81c1c8e7a801d583e0e9e4d4c460161
raihanputro <raihanputromaulana477@gmail.com>
2022-12-03 21:03:18 +0700

    Adding some route in app

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Container } from '@material-ui/core';
 import { useDispatch } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import useStyles from './styles';
 import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
-
+import Auth from "./components/auth/auth";
+import FishPostDetail from "./components/fishPosts/fishPostDetail/fishPostDetail";
 
 const App = () => {
+
+    const user = JSON.parse(localStorage.getItem('profile'));
+
     return (
         <BrowserRouter>
             <Container maxWidth="lg">
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={<Navigate to="/fishes" />} />
+                    <Route path="/fishes" element={<Home />} />
+                    <Route path="/fishes/search" element={<Home />} />
+                    <Route path="/fishes/:id" element={<FishPostDetail />} />
+                    <Route path="/auth" element={!user ? <Auth /> : <Navigate to="/fishes" />}/>
                 </Routes>
             </Container>
         </BrowserRouter>
