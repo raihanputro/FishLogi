5cec3df8b0b4c62fe99da1154562892ffde84636
raihanputro <raihanputromaulana477@gmail.com>
2022-12-03 21:03:03 +0700

    Adding detail post and search api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/fishes';
+const API = axios.create({ baseURL: 'http://localhost:5000'});
 
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
 
-export const fecthFishPost = () => axios.get(url);
-export const createFishPost = (newFishPost) => axios.post(url, newFishPost);
-export const updateFishPost = (id, updatedFishPost) => axios.patch(`${url}/${id}`, updatedFishPost);
-export const deleteFishPost = (id) => axios.delete(`${url}/${id}`)
+    return req;
+});
+
+export const fetchFishPostsBySearch = (searchQuery) => API.get(`/fishes/search?searchQuery=${searchQuery.search}`);
+export const fetchFishPost = (id) => API.get(`/fishes/${id}`);
+export const fetchFishPosts = () => API.get('/fishes');
+export const createFishPost = (newFishPost) => API.post('/fishes', newFishPost);
+export const updateFishPost = (id, updatedFishPost) => API.patch(`/fishes/${id}`, updatedFishPost);
+export const deleteFishPost = (id) => API.delete(`/fishes/${id}`);
+
+export const signIn = (formData) => API.post('/users/signIn', formData);
+export const signUp = (formData) => API.post('/users/signUp', formData);
