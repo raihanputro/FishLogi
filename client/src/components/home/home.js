db373a05c4251851a29c6d058c68e00ea726864f
raihanputro <raihanputromaulana477@gmail.com>
2023-01-06 15:13:27 +0700

    Adding fish post and users table to home

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useSelector } from "react";
-import { CSVLink } from 'react-csv';
 import { Container, Grow, Grid, AppBar, TextField, Button } from "@material-ui/core";
+import { Paper } from "@material-ui/core"
 import { useDispatch } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import FishPosts from "../fishPosts/fishPosts";
-import Form from "../form/form";
 import useStyles from './styles';
 import { getFishPosts, getFishPostsBySearch } from "../../actions/fishPosts";
+import FishPostsTable from "../fishPosts/fishPostsTable/fishPostTable";
+import UsersTable from "../users/usersTable/usersTable";
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -17,11 +18,11 @@ const Home = () => {
     const query = useQuery();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [fishData, setFishData] = useState([]);
-    const [currentId, setCurrentId] = useState(null);
+    const page = query.get('page') || 1;
+    const [currentId, setCurrentId] = useState(0);
+    const searchQuery = query.get('searchQuery');
     const [search, setSearch] = useState('');
     const user = JSON.parse(localStorage.getItem('profile'));
-    const searchQuery = query.get('searchQuery');
 
     const searchFishPosts = () => {
         if(search.trim()) {
@@ -39,52 +40,44 @@ const Home = () => {
     }
 
     useEffect(() => {
-        dispatch(getFishPosts(setFishData));
+        dispatch(getFishPosts());
     }, [currentId, dispatch]);
 
-    const headers = [
-        {label: 'Nama Ikan', key: 'name'},
-        {label: 'Nama Latin Ikan', key: 'latinName'},
-        {label: 'Kelas Ikan', key: 'classes'},
-        {label: 'Spesies Ikan', key: 'species'},
-        {label: 'Tipe Ikan', key: 'type'},
-        {label: 'Deskripsi Ikan', key: 'desc'},
-        {label: 'Habitat Ikan', key: 'habitats'},
-        {label: 'Daerah Endemik Ikan', key: 'endemicArea'},
-        {label: 'Foto Ikan', key: 'fishPicture'},
-        {label: 'Id Penulis', key: 'authorId'},
-        {label: 'Nama Penulis', key: 'authorName'},
-    ]
-
+ 
     return (
         <Grow in maxWidth="lg">
             <Container className={classes.container}>
-                <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <FishPosts setCurrentId={setCurrentId} />
+                {user?.result?.isAdmin === 'admin' ? (
+                    <>
+                        <FishPostsTable />
+                        <UsersTable />
+                    </>
+                ) : null}
+                {user?.result?.isAdmin === 'admin' ? null : (
+                    <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={2}>
+                        <Grid item xs={12} sm={7}>
+                            <FishPosts setCurrentId={setCurrentId} />
+                        </Grid>
+                        <Grid item xs={12} sm={4} >
+                            <Paper className={classes.paper}>
+                                <TextField 
+                                    name="search" 
+                                    label="Cari Ikan" 
+                                    variant="outlined" 
+                                    fullWidth 
+                                    value={search}
+                                    onChange={(e) => setSearch(e.target.value)}  
+                                    onKeyDown={handleKeyPress}
+                                />
+                                <Button style={{ marginTop: '5px'}} variant="contained" size="large" color="primary" type="submit" fullWidth onClick={searchFishPosts}>Cari</Button>
+                            </Paper>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <CSVLink data={fishData} headers={headers} target="_blank" filename="Data Ikan_Fishlogi" className={classes.excel} >Download Data Ikan</CSVLink> 
-                        <AppBar className={classes.search} position="static" color="inherit" elevation={6}>
-                            <TextField 
-                                name="search" 
-                                label="Cari Ikan" 
-                                variant="outlined" 
-                                fullWidth 
-                                value={search}
-                                onChange={(e) => setSearch(e.target.value)}  
-                                onKeyDown={handleKeyPress}
-                            />
-                            <Button variant="contained" size="large" color="primary" type="submit" fullWidth onClick={searchFishPosts}>Cari</Button>
-                        </AppBar>
-                        {user?.result?.isAdmin === 'admin' && (
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        )}
-                    </Grid>
-                </Grid>
+                )}
             </Container>
         </Grow>
     )
 }
 
 export default Home;
+
--- client/src/components/home/styles.js
+++ client/src/components/home/styles.js
@@ -1,11 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    [theme.breakpoints.down('sm')]: {
-        mainContainter: {
-            flexDirection: 'column-reverse',
-        }
+  mainContainer: {
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column-reverse',
     },
+  },
     container: {
         marginTop: '130px'
     },
@@ -13,14 +13,16 @@ export default makeStyles((theme) => ({
         marginTop: '10px',
         marginBottom: '20px'
     },
-    excel: {
-        backgroundColor: '#8dc63f',
-        fontSize: 14,
-        fontWeight: 500,
-        height: "100px",
-        padding: '0 48px',
-        borderRadius: 5,
-        color: '#fff',
-        textDecoration: 'none'
+    root: {
+        width: '100%',
+      },
+    container1: {
+      maxHeight: 440,
+      borderRadius: "5px"
+    },
+    paper: {
+      padding: theme.spacing(2),
+      borderRadius: '15px',
+      marginBottom: '10px'
     }
 }))
