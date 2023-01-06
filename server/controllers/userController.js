d94ca10af46af86c51b9909f3b6b3fbc7a74a184
raihanputro <raihanputromaulana477@gmail.com>
2023-01-06 14:53:25 +0700

    Adding get users feature in server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,8 +4,18 @@ import usersModel from "../models/usersModel.js";
 
 const secret = 'test';
 
+export const getUsers = async(req, res) => {
+    try {
+        const users = await usersModel.findAll();
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 export const signIn = async(req, res) => {
-    const { email, password } = req.body;
+    const {email, password, loginAt} = req.body;
 
     try {
         const existingUser = await usersModel.findOne({
@@ -14,15 +24,21 @@ export const signIn = async(req, res) => {
             }    
         });
 
+        await usersModel.update({loginAt: new Date().toISOString()}, {
+            where: {
+                email: req.body.email
+            }
+        })
+
         if(!existingUser) return res.status(404).json({message: "Email belum terdaftar, silahkan ke halaman Daftar!"});
 
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
         if(!isPasswordCorrect) return res.status(400).json({ message: "Password salah! silahkan masukkan password yang benar!"});
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser.id}, secret, {expiresIn: "1h"});
+        const token = jwt.sign({ email: existingUser.email, id: existingUser.id }, secret, {expiresIn: "1h"});
 
-        res.status(200).json({ result: existingUser, token});
+        res.status(200).json({ result: existingUser, token });
     } catch (error) {
         res.status(500).json({ message: "Tidak dapat masuk!"});
         console.log(error);
@@ -52,10 +68,23 @@ export const signUp = async(req, res) => {
 
         const token = jwt.sign({ email: result.email, id: result.id}, secret, {expiresIn:"1h"});
 
-        res.status(201).json({ result, token });
+        res.status(201).json({ result });
     } catch (error) {
         res.status(500).json({ message: "Tidak dapat daftar!"});
         console.log(error);
     }
 }
 
+export const logOut = async(req, res) => {
+    try {
+        const logOutedUser = await usersModel.update({logoutAt: new Date().toISOString()}, {
+            where: {
+                id: req.params.id
+            },
+        });
+
+        res.json(logOutedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signIn, signUp } from '../controllers/userController.js';
+import { getUsers, logOut, signIn, signUp } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signIn', signIn);
 router.post('/signUp', signUp);
+router.get('/', getUsers);
+router.patch('/:id', logOut);
 
 export default router;
